05a16e17f80b5151a1839c2f976c6d7f13c6b8a2
中田映理 <er1d0n5963@gmail.com>
2019-06-28 23:08:20 +0900

    モックアップのレイアウト

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,25 @@
+class MembersController < ApplicationController
+　#会員番号
+  def index
+    @members=Member.order("number")
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root "top#index"
   get "about" => "top#about", as: "about"
+
   #Lessonコントローラのstep1~18までのアクションを呼び出す
 1.upto(18) do |n|
   get "lesson/step#{n}(/:name)" => "lesson#step#{n}"
 end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :members #Ｍｅｍｂｅｒｓコントローラに対して７つのアクションのルーティングが設定可能になる
+
 end
