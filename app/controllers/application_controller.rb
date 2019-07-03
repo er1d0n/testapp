466f157a22ca6440e327f05619b9aadb633b7756
中田映理 <er1d0n5963@gmail.com>
2019-07-03 23:09:51 +0900

    アクセス制限機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
  end
  #引数に指定された名前のメソッドをテンプレートの中でも使えるメソッド（ヘルパーメソッド）として登録
  helper_method :current_member
+
+
+ class LoginRequired < StandardError; end
+ class Forbidden < StandardError; end
+
+ private def login_required
+   raise LoginRequired unless current_member
+ end
 end
